01926f5c38b52d180b00887c20d107f89a2c57fa
Blackwidow <work.luanacordeiro@gmail.com>
2025-05-27 11:18:11 -0300

    feat(#2): setting up ´ESLnt & Prettier´ for code quality

--- react-site/src/App.jsx
+++ react-site/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card, CardContent } from "./components/ui/card";
 import { Button } from "./components/ui/button";
-import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
